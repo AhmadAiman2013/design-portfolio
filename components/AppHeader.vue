9876ba7ce716bf80f91d6b19676349f78000b656
ahmad aiman <ahmadaimanalias@gmai.com>
2024-07-16 22:06:42 +0800

    first modi-desktop

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,23 +1,33 @@
 <template>
     <div class="shrink-0 flex w-full h-[61px] max-w-[1116px] justify-between items-end mx-auto py-1 mt-[8px]">
         <div class="flex gap-3 items-end">
-            <NuxtLink to="/">
-                <h1 class="text-2xl">Ahmad Aiman Alias</h1>
-            </NuxtLink>
-            <div >
-                <Icon name="radix-icons:github-logo" size="1.75em"/>
+            <div class="border-b-4 border-primary hover:bg-primary hover:text-white dark:hover:text-black">
+                <NuxtLink to="/">
+                    <h1 class="text-2xl">Ahmad Aiman Alias</h1>
+                </NuxtLink>
             </div>
-            <div>
-                <Icon name="radix-icons:linkedin-logo" size="1.75em"/>
+            <div class="hover:bg-primary hover:text-white dark:hover:text-black">
+                <NuxtLink>
+                    <Icon name="radix-icons:github-logo" size="1.75em" />
+                </NuxtLink>
+            </div>
+            <div class="hover:bg-primary hover:text-white dark:hover:text-black">
+                <NuxtLink>
+                    <Icon name="radix-icons:linkedin-logo" size="1.75em" />
+                </NuxtLink>
             </div>
         </div>
         <div class="flex text-lg gap-6 items-end">
-            <NuxtLink to="/projects">
-                <h1><span>/</span>projects</h1>
-            </NuxtLink>
-            <NuxtLink to="/blogs">
-                <h1><span>/</span>blogs</h1>
-            </NuxtLink>
+            <h1 class="border-b-4 border-primary hover:bg-primary hover:text-white dark:hover:text-black">
+                <NuxtLink to="/projects">
+                    <span>/</span>projects
+                </NuxtLink>
+            </h1>
+            <h1 class="border-b-4 border-primary hover:bg-primary hover:text-white dark:hover:text-black" >
+                <NuxtLink to="/blogs">
+                    <span>/</span>blogs
+                </NuxtLink>
+            </h1>
             <div>
                 <ThemeToggle />
             </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,46 @@
 <script setup lang="ts">
 import { VueTypedJs } from 'vue3-typed-ts';
 
-const positions = ['Frontend?', 'Backend?', 'Fullstack?', 'DevOps?'];
+const positions = ['Frontend', 'Backend', 'Fullstack', 'DevOps'];
+
+// frontend
+const frontend = ['javascript', 'typescript', 'php', 'c-sharp', 'python', 'react', 'nextjs', 'vue', 'nuxt'];
+
+//backend
+const backend = [
+  'nodedotjs',
+  'express',
+  'nestjs',
+  'laravel',
+  'dotnet',
+  'fastapi',
+  'mongodb',
+  'postgresql',
+  'mysql',
+  'sqlite',
+  'amazondynamodb',
+  'microsoftsqlserver',
+];
+
+//styling
+const styling = ['tailwindcss', 'postcss', 'bootstrap', 'daisyui', 'shadcnui'];
+
+//others
+// cloudflare,prisma,drizzle,git,figma,docker,terraform,aws,gcp,azure,langchain,pytorch
+const others = ['cloudflare', 'prisma', 'drizzle', 'git', 'figma', 'langchain', 'pytorch', 'docker', 'terraform', 'amazonaws', 'googlecloud',];
 </script>
 
 <template>
   <section class="w-full max-w-[1116px] mx-auto mt-12">
     <div class="flex justify-between">
       <div class="flex-1 text-xl">
-        <h1 class="text-4xl mb-5">Pleased to meet you.</h1>
-        <h2>Code. Create. and Code again.</h2>
+        <h1 class="text-4xl mb-5">Pleased to meet you<span class="text-7xl text-primary">.</span></h1>
         <VueTypedJs :strings=positions :typeSpeed="100" :backSpeed="100" :loop="true">
-          <h2>Looking for
-            <span class="typing"></span>
-          </h2>
+          <h1 class="text-2xl" >Aspired to be a
+            <span class="typing text-primary"></span>
+          </h1>
         </VueTypedJs>
-        <p class="mt-5 text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque eius laboriosam
+        <p class="mt-2 text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque eius laboriosam
           deleniti sed, at in modi atque
           magnam laborum error. Eveniet qui sapiente harum voluptate voluptas in deserunt tenetur eius.</p>
       </div>
@@ -23,35 +48,76 @@ const positions = ['Frontend?', 'Backend?', 'Fullstack?', 'DevOps?'];
         <NuxtImg src="/self_picture.png" alt="Ahmad Aiman Alias" width="300" height="300" class="rounded-md" />
       </div>
     </div>
-    <div class="mt-10 text-base">
-      <h1 class="text-3xl">Skills.</h1>
+    <div class="max-w-[1000px] mt-10 text-base">
+      <h1 class="text-3xl">Skills<span class="text-6xl text-primary">.</span></h1>
       <div class="my-2">
-        <h1>
+        <h1 class="underline decoration-primary decoration-4" >
           #Frontend
         </h1>
-        
+        <div class="grid grid-cols-6 mt-2">
+          <li v-for="item in frontend" :key="item" class="flex flex-col m-2">
+            <Icon :name="`tabler:brand-${item}`" size="3rem" />
+            {{ item }}
+          </li>
+        </div>
       </div>
-      <div class="my-2">
-        <h1>
+      <div class="my-3">
+        <h1 class="underline decoration-primary decoration-4" >
           #Backend
         </h1>
+        <div class="grid grid-cols-6 mt-2">
+          <li v-for="item in backend" :key="item" class="flex flex-col m-2">
+            <Icon :name="`simple-icons:${item}`" size="3rem" />
+            {{ item }}
+          </li>
+        </div>
       </div>
-      <div class="my-2">
-        <h1>
+      <div class="my-3">
+        <h1 class="underline decoration-primary decoration-4" >
           #Styling
         </h1>
+        <div class="grid grid-cols-6 mt-2">
+          <li v-for="item in styling" :key="item" class="flex flex-col m-2">
+            <Icon :name="`simple-icons:${item}`" size="3rem" />
+            {{ item }}
+          </li>
+        </div>
       </div>
-      <div class="my-2">
-        <h1>
+      <div class="my-3">
+        <h1 class="underline decoration-primary decoration-4" >
           #Others
         </h1>
+        <div class="grid grid-cols-6 mt-2">
+          <li v-for="item in others" :key="item" class="flex flex-col m-2">
+            <Icon :name="`simple-icons:${item}`" size="3rem" />
+            {{ item }}
+          </li>
+          <li class="flex flex-col m-2">
+            <Icon name="tabler:brand-azure" size="3rem" />
+            azure
+          </li>
+        </div>
+
       </div>
     </div>
     <div class="mt-10 mb-20">
-      <h1 class="text-3xl">Contacts.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora nisi nesciunt quibusdam totam suscipit ducimus
-        possimus deserunt eius unde voluptatem temporibus similique odio, laboriosam, natus, voluptatum perferendis
-        repellat consequatur omnis?</p>
+      <h1 class="text-3xl">Contacts<span class="text-7xl text-primary">.</span></h1>
+      <div class="flex justify-between" >
+        <p class="max-w-[500px] my-2">
+          Currently looking for fulltime/freelance. However, if you have other request or question, don’t hesitate to
+          contact me</p>
+        <div>
+          <p class="text-primary" >Message me here</p>
+          <div class="flex items-center space-x-2" >
+            <Icon name="radix-icons:envelope-closed" size="1rem"/>
+            <p>ahmadaimanalias.gmail</p>  
+          </div>
+          <div class="flex items-center space-x-2" >
+            <Icon name="radix-icons:linkedin-logo" size="1rem"/>
+            <p>ahmadaiman</p>  
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
